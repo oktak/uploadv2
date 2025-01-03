1dc8bed63d5793384de6d323e16179175ed65a2a
oktak <oktak@users.noreply.github.com>
2025-01-03 19:58:29 +0800

    Add GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,23 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Load environment variables
+import dotenv from 'dotenv';
+dotenv.config();
+
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the
   // `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), '');
+  const BASEURL = process.env.VITE_BASEURL;
+
   return {
     // vite config
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
-    base: env.VITE_BASEURL || `http://127.0.0.1:5500/dist/`,
+    base: env.VITE_BASEURL || BASEURL || `http://127.0.0.1:5500/dist/`,
     plugins: [react()],
   };
 });
